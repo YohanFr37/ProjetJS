3647c05dfae5979ff54129520a49d22c430e78e5
Lellundril <GuillaumeLetzelter@gmail.com>
2022-11-19 18:31:09 +0100

    server.js reads the HTML file and create server

---
+++ server.js
@@ -0,0 +1,39 @@
+// content of index.js
+var http = require('http');
+var fs = require('fs'); // FileSystem to read the HTML file
+
+const PORT=8080; // Port the server wil be listening on
+
+fs.readFile('./index.html', function (err, html) {
+
+    if (error) throw error;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  // Load the HTML file
+        response.end();  
+    }).listen(PORT); // Listen on the specified port
+});
+
+/*
+
+const http = require('http')
+const port = 8080 // Listening port for the server
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  response.write("yo")
+  response.end()
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (error) => {
+  if (error) {
+    return console.log('something bad happened', error)
+  }
+  console.log(`server is listening on ${port}`)
+})
+
+*/
